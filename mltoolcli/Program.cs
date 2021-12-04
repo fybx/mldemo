404214ecee65c6472dd6241422eb1feaf253f427
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2021-12-04 17:14:31 +0300

    Base implementation for KomutIsle()

--- mltoolcli/Program.cs
+++ mltoolcli/Program.cs
@@ -4,21 +4,40 @@ namespace mltoolcli
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             bool quit = false;
             while (!quit)
             {
                 Console.Title = @"user@mltool";
-                Console.WriteLine("user@mltool: ");
-                string[] tokens = Console.ReadLine()?.Trim().Split(' ');
-                quit = KomutIsle(tokens);
+                Console.Write("user@mltool: ");
+                quit = KomutIsle(Console.ReadLine()?.Trim().Split(' '));
             }
         }
 
-        static bool KomutIsle(string[] tokens)
+        private static bool KomutIsle(string[] tokens)
         {
-            throw new NotImplementedException();
+            switch (tokens[0])
+            {
+                case "help":
+                    Console.WriteLine("\nmltool cli v0010");
+                    Console.WriteLine("exit                : CLI'ten çıkar");
+                    Console.WriteLine("help                : Bu mesajı gösterir");
+                    Console.WriteLine("train               : Yüklü olan eğitim veri setini kullanarak yüklü olan modeli eğitir.");
+                    Console.WriteLine("test                : Yüklü olan test veri setini kullanarak yüklü olan modeli test eder.");
+                    Console.WriteLine("load <model/dataset>: Model veya veri setini train/test aşamasında kullanılmak üzere programa yükler.");
+                    Console.WriteLine("new <model/dataset> : Yeni bir model veya veri seti dosyası oluşturur. Dosya programın çalıştırıldığı yolda konumlandırılacaktır.");
+                    break;
+                
+                case "exit":
+                    return true;
+                
+                case "default":
+                    Console.WriteLine(@"mltool: ""{0}"" geçerli bir komut değil", tokens[0]);
+                    break;
+            }
+
+            return false;
         }
     }
 }
