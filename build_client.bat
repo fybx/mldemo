76f416acf6527cec96e7cab1e8bb3d4b2f765159
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2021-12-13 23:46:24 +0300

    Rewrote windows build script for mltoolcli

--- build_client.bat
+++ build_client.bat
@@ -1,19 +1,38 @@
+@echo off
+title mltoolcli build
+
 Rem mltoolcli build script for Windows
 
-@echo off
+Rem creates artifacts folder if not exists
+if not exist "%~dp0\artifacts\" mkdir %~dp0\artifacts
 
 echo:
-echo Building mltoolcli
+echo 1. Clean up artifacts folder
+echo:
+del /s /q "%~dp0\artifacts\*.*"
+
+echo:
+echo 2. Clean up build output folder
+echo:
+del /s /q "%~dp0\mltoolcli\bin\Release\net6.0\*.*"
+
+echo:
+echo 3. Build mltoolcli for Release
 echo:
 dotnet build --nologo --verbosity minimal --configuration Release
 
 echo:
-echo Copy pyscripts folder to build artifacts
+echo 4. Copy mltoolcli to artifacts
+echo:
+xcopy %~dp0\mltoolcli\bin\Release\net6.0 %~dp0\artifacts
+
+echo:
+echo 5. Copy pyscripts folder to artifacts
 echo:
 Rem creates pyscripts folder in build folder if not exists
-if not exist "%~dp0\mltoolcli\bin\Release\net6.0\pyscripts\" mkdir %~dp0\mltoolcli\bin\Release\net6.0\pyscripts
+if not exist "%~dp0\artifacts\pyscripts\" mkdir %~dp0\artifacts\pyscripts
 Rem copies scripts to previously created folder
-xcopy %~dp0\pyscripts %~dp0\mltoolcli\bin\Release\net6.0\pyscripts
+xcopy %~dp0\pyscripts %~dp0\artifacts\pyscripts
 
 echo:
 echo:
