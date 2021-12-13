19ae8168b22dce8b66db6d156ec8e9888c72598d
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2021-12-13 23:30:22 +0300

    Updated bash script to suit new repo structure

--- build_mltoolcli.sh
+++ build_mltoolcli.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+mkdir -p artifacts
+
+echo "Clean-up artifacts"
+cd artifacts
+rm -rf *
+cd ..
+
 echo "Building mltoolcli"
 dotnet build --nologo --verbosity minimal --configuration Release
 
-echo "Copy pyscripts/ to build artifacts"
-cp -ar pyscripts mltoolcli/bin/Release/net6.0
+echo "Copy build artifacts to artifacts folder"
+cp -ar mltoolcli/bin/Release/net6.0/. artifacts
+
+echo "Copy pyscripts folder to artifacts folder"
+cp -ar pyscripts artifacts
 
 echo "Process complete. You can run the application with './mltoolcli' or 'sh run_mltoolcli.sh'"
