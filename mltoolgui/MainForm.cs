225a45fe8da5dab818b953b29dbac432182eec1b
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2021-12-14 23:43:29 +0300

    Added logic scaffolding

--- mltoolgui/MainForm.cs
+++ mltoolgui/MainForm.cs
@@ -1,10 +1,63 @@
 // ReSharper disable once IdentifierTypo
+using System.Diagnostics;
 namespace mltoolgui;
 
 public partial class MainForm : Form
 {
+    private string? pathModelFile;
+    private string? pathDatasetFile;
+
+    private double[]? model;
+    private int[]? dataset;
+
+    public MainForm() => InitializeComponent();
     public MainForm()
     {
         InitializeComponent();
+    private static void CallScript(string scriptName, string arguments)
+    {
+        ProcessStartInfo info = new()
+        {
+            FileName = "python3",
+            Arguments = $"{AppContext.BaseDirectory}pyscripts\\{scriptName}.py {arguments}"
+        };
+        Process.Start(info);
+    }
+
+    private double Calculate(double number)
+    {
+        if (string.IsNullOrEmpty(pathModelFile) || string.IsNullOrEmpty(pathDatasetFile))
+            MessageBox.Show("Add a data set and a model file to start", "Warning!");
+        else
+        {
+            double result = 0;
+            for (int i = 0; i < 6; i++)
+                result += model[i] * Math.Pow(number, 5 - i);
+            return result;
+        }
+        return double.NaN;
+    }
+
+    private bool ValidateFile(string path)
+    {
+        List<string> lines = File.ReadAllLines(path).ToList();
+        switch (lines.Count)
+        {
+            case 8 when lines[0] is @"mltool modeli":
+                model = new double[6];
+                for (int i = 2; i < 8; i++)
+                    model[i - 2] = double.Parse(lines[i]);
+                return true;
+
+            case 66 when lines[0] is @"mltool veri seti":
+                dataset = new int[64];
+                for (int j = 2; j < 66; j++)
+                    dataset[j - 2] = int.Parse(lines[j]);
+                return true;
+
+            default:
+                MessageBox.Show($"File {path} is not recognized. Model or data set file must be selected!", "Warning!");
+                return false;
+        }
     }
 }
