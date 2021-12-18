d6e78b8da7f61666010da4e46490e9bc5dd0e5d6
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2021-12-18 21:01:59 +0300

    Removed CallScript(), added RunMltoolcli(). Moved logic from event methods to new methods

--- mltoolgui/MainForm.cs
+++ mltoolgui/MainForm.cs
@@ -16,15 +16,32 @@ public partial class MainForm : Form
 
     private void MainForm_Load(object sender, EventArgs e) => Where();
 
-    private void btnTrain_Click(object sender, EventArgs e)
+    private void btnTrain_Click(object sender, EventArgs e) => Train();
+
+    private void btnEvaluate_Click(object sender, EventArgs e) => Evaluate();
+
+    private static void RunMltoolcli(string command, string arguments)
+    {
+        ProcessStartInfo info = new()
+        {
+            FileName = "mltoolcli.exe",
+            Arguments = $"{command} {arguments}"
+        };
+        Process prc = Process.Start(info);
+        prc.WaitForExit();
+        if (prc.ExitCode is not 0 or -1000)
+            MessageBox.Show($"An error has occured.\n mltoolcli.exe returned status code: {prc.ExitCode}.", "Important Warning!");
+    }
+
+    private void Train()
     {
         if (string.IsNullOrEmpty(pathModelFile) || string.IsNullOrEmpty(pathDatasetFile))
             MessageBox.Show("Add a data set and a model file to start", "Warning!");
         else
-            CallScript("trainmodel", $"{pathDatasetFile} {pathModelFile}");
+            RunMltoolcli("train", $"{pathDatasetFile} {pathModelFile}");
     }
 
-    private void btnEvaluate_Click(object sender, EventArgs e)
+    private void Evaluate()
     {
         if (string.IsNullOrEmpty(pathModelFile))
             MessageBox.Show("Add a model file to start", "Warning!");
@@ -35,14 +52,23 @@ public partial class MainForm : Form
         }
     }
 
-    private static void CallScript(string scriptName, string arguments)
+    private void Load(string what)
     {
-        ProcessStartInfo info = new()
+        switch (what)
         {
-            FileName = "python3",
-            Arguments = $"{AppContext.BaseDirectory}pyscripts\\{scriptName}.py {arguments}"
-        };
-        Process.Start(info);
+            case "model" or "dataset":
+                OpenFileDialog ofd = what is "model" 
+                    ? new() { Filter = "Model file|*.model", Title = "Load a model file" } 
+                    : new() { Filter = "Data set file|*.dataset", Title = "Load a data set file" };
+
+                if (ofd.ShowDialog() is DialogResult.OK)
+                    ValidateFile(ofd.FileName);
+                break;
+
+            default:
+                MessageBox.Show($"MainForm.Load(): value of argument {nameof(what)} must be 'model' or 'dataset'");
+                break;
+        }
     }
 
     private double Calculate(double number)
@@ -91,8 +117,11 @@ public partial class MainForm : Form
     {
         string run = AppContext.BaseDirectory;
         string pyt = $"{run}pyscripts\\";
+        string cli = $"{run}mltoolcli.exe";
         if (!Directory.Exists(pyt))
             MessageBox.Show("mltoolgui could not locate \'pyscripts\' folder in running directory. Have you used build script to compile this application?", "Fatal Error!");
+        if (File.Exists(cli) is false)
+            MessageBox.Show("mltoolgui could not locate \'mltoolcli.exe\' in running directory. Have you used build script to compile this application?", "Fatal Error!");
     }
 
     private void textBox1_KeyDown(object sender, KeyEventArgs e)
@@ -129,49 +158,21 @@ public partial class MainForm : Form
     {
         using InputDialog dialog = new("dataset");
         DialogResult result = dialog.ShowDialog();
-        if (result is DialogResult.OK && dialog.FileName is not null)
-        {
-            string path = $"{AppContext.BaseDirectory}{(dialog.FileName.EndsWith(".dataset") ? dialog.FileName : $"{dialog.FileName}.dataset")}";
-            File.Create(path).Close();
-            CallScript(@"newdataset", path);
-        }
+        if (result is DialogResult.OK && string.IsNullOrWhiteSpace(dialog.FileName))
+            RunMltoolcli("new", $"dataset {dialog.FileName}");
     }
 
     private void tsmiNewModel_Click(object sender, EventArgs e)
     {
         using InputDialog dialog = new("model");
         DialogResult result = dialog.ShowDialog();
-        if (result is DialogResult.OK && dialog.FileName is not null)
-        {
-            string path = $"{AppContext.BaseDirectory}{(dialog.FileName.EndsWith(".model") ? dialog.FileName : $"{dialog.FileName}.model")}";
-            File.Create(path).Close();
-            CallScript(@"newmodel", path);
-        }
+        if (result is DialogResult.OK && string.IsNullOrWhiteSpace(dialog.FileName))
+            RunMltoolcli("new", $"model {dialog.FileName}");
     }
 
-    private void tsmiLoadDataset_Click(object sender, EventArgs e)
-    {
-        OpenFileDialog ofd = new()
-        {
-            Filter = "Data set file|*.dataset",
-            Title = "Load a data set file"
-        };
+    private void tsmiLoadDataset_Click(object sender, EventArgs e) => Load("dataset");
 
-        if (ofd.ShowDialog() is DialogResult.OK)
-            ValidateFile(ofd.FileName);
-    }
-
-    private void tsmiLoadModel_Click(object sender, EventArgs e)
-    {
-        OpenFileDialog ofd = new()
-        {
-            Filter = "Model file|*.model",
-            Title = "Load a model file"
-        };
-
-        if (ofd.ShowDialog() is DialogResult.OK)
-            ValidateFile(ofd.FileName);
-    }
+    private void tsmiLoadModel_Click(object sender, EventArgs e) => Load("model");
 
     private void tsmiLoadBundle_Click(object sender, EventArgs e)
     {
@@ -198,7 +199,7 @@ public partial class MainForm : Form
         }
     }
 
-    private void tsmiTrain_Click(object sender, EventArgs e) => btnTrain_Click(sender, e); // dirty but it works
+    private void tsmiTrain_Click(object sender, EventArgs e) => Train();
 
     private void tsmiEvaluate_Click(object sender, EventArgs e) => btnEvaluate_Click(sender, e);
 
