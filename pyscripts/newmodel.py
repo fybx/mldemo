3d2d0203ebf694db2f675c130a1bba6d0ad5ec22
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2021-12-19 01:43:34 +0300

    Update newmodel.py

--- pyscripts/newmodel.py
+++ pyscripts/newmodel.py
@@ -5,19 +5,19 @@ import os
 def main():
     print("mltool (pyscripts): [BİLGİ] yeni model yaratılıyor!")
     try:
-        fileloc = sys.argv[1]   # cli tarafından yaratılan model dosyasının tam konumu
+        file_path = sys.argv[1]   # cli tarafından yaratılan model dosyasının tam konumu
     except IndexError:
-        print("mltool (pyscripts): [HATA] newmodel.py fileloc argümanı bekliyor.")
+        print("mltool (pyscripts): [HATA] newmodel.py file_path argümanı bekliyor.")
         sys.exit()
 
-    with open(fileloc, 'w') as f:
+    with open(file_path, 'w') as f:
         f.write("mltool modeli\n")
-        f.write(os.path.basename(fileloc).split('.')[0] + '\n')
+        f.write(os.path.basename(file_path).split('.')[0] + '\n')
         for i in range(6):
             f.write("1\n")
 
     print("mltool (pyscripts): [BİLGİ] model yaratıldı.")
-    print("Konum: %s" % fileloc)
+    print(f"Konum: {file_path}")
 
 
 main()
