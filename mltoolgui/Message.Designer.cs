66dc29555652b668abddb265d10f1b830c5fc3b5
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2021-12-19 00:09:09 +0300

    Added custom message box

---
+++ mltoolgui/Message.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace mltoolgui
+{
+    partial class Message
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.ButtonOK = new System.Windows.Forms.Button();
+            this.ButtonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Location = new System.Drawing.Point(9, 9);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(0);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Padding = new System.Windows.Forms.Padding(0, 0, 9, 9);
+            this.lblMessage.Size = new System.Drawing.Size(350, 89);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "<Message: None>";
+            // 
+            // ButtonOK
+            // 
+            this.ButtonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.ButtonOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonOK.Location = new System.Drawing.Point(281, 101);
+            this.ButtonOK.Margin = new System.Windows.Forms.Padding(3, 3, 9, 9);
+            this.ButtonOK.Name = "ButtonOK";
+            this.ButtonOK.Size = new System.Drawing.Size(75, 23);
+            this.ButtonOK.TabIndex = 1;
+            this.ButtonOK.Text = "OK";
+            this.ButtonOK.UseVisualStyleBackColor = true;
+            // 
+            // ButtonCancel
+            // 
+            this.ButtonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ButtonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonCancel.Location = new System.Drawing.Point(194, 101);
+            this.ButtonCancel.Margin = new System.Windows.Forms.Padding(3, 3, 3, 9);
+            this.ButtonCancel.Name = "ButtonCancel";
+            this.ButtonCancel.Size = new System.Drawing.Size(75, 23);
+            this.ButtonCancel.TabIndex = 2;
+            this.ButtonCancel.Text = "Cancel";
+            this.ButtonCancel.UseVisualStyleBackColor = true;
+            // 
+            // Message
+            // 
+            this.AcceptButton = this.ButtonOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.CancelButton = this.ButtonCancel;
+            this.ClientSize = new System.Drawing.Size(369, 136);
+            this.Controls.Add(this.ButtonCancel);
+            this.Controls.Add(this.ButtonOK);
+            this.Controls.Add(this.lblMessage);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Message";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "<Title: None>";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label lblMessage;
+        private Button ButtonOK;
+        private Button ButtonCancel;
+    }
+}
---
+++ mltoolgui/Message.cs
@@ -0,0 +1,32 @@
+﻿namespace mltoolgui
+{
+    public partial class Message : Form
+    {
+        public Message(string title, string message)
+        {
+            InitializeComponent();
+            Text = title;
+            lblMessage.Text = message;
+            ButtonCancel.Visible = false;
+        }
+
+        public Message(string title, string message, bool cancellable = true)
+        {
+            Text = title;
+            lblMessage.Text = message;
+            ButtonCancel.Visible = cancellable;
+        }
+
+        public static DialogResult ShowDialog(string title, string message)
+        {
+            using Message m = new(title, message);
+            return m.ShowDialog();
+        }
+
+        public static DialogResult ShowDialog(string title, string message, bool cancellable = true)
+        {
+            using Message m = new(title, message, cancellable);
+            return m.ShowDialog();
+        }
+    }
+}
