00dc16bc0ab85d8d6c65b7612cf71feabbe03bc3
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2021-12-19 01:41:11 +0300

    Update newdataset.py

--- pyscripts/newdataset.py
+++ pyscripts/newdataset.py
@@ -2,33 +2,35 @@ import random
 import sys
 import os
 
-print("mltool (pyscripts): [BİLGİ] yeni veri seti yaratılıyor!")
-try:
-    fileloc = sys.argv[1]   # cli tarafından yaratılan veri seti dosyasının tam konumu
-except IndexError:
-    print("mltool (pyscripts): [HATA] newdataset.py fileloc argümanı bekliyor.")
-    sys.exit()
 
-xvals = []
-yvals = []
+def main():
+    print("mltool (pyscripts): [BİLGİ] yeni veri seti yaratılıyor!")
+    try:
+        file_path = sys.argv[1]  # cli tarafından yaratılan veri seti dosyasının tam konumu
+    except IndexError:
+        print("mltool (pyscripts): [HATA] newdataset.py file_path argümanı bekliyor.")
+        sys.exit()
 
-xval = 1
-while xval < 1025:
-    yval = random.randint(1, 512)
-    xvals.append(xval)
-    yvals.append(yval)
-    xval += 32
+    x_values = []
+    y_values = []
 
-xlen = len(xvals)
-ylen = len(yvals)
+    x_value = 1
+    while x_value < 1025:
+        y_value = random.randint(1, 512)
+        x_values.append(x_value)
+        y_values.append(y_value)
+        x_value += 32
 
-with open(fileloc, 'w') as f:
-    f.write("mltool veri seti\n")
-    f.write(os.path.basename(fileloc).split('.')[0] + '\n')
-    for item in xvals:
-        f.write("%s\n" % item)
-    for item in yvals:
-        f.write("%s\n" % item)
+    with open(file_path, 'w') as f:
+        f.write("mltool veri seti\n")
+        f.write(os.path.basename(file_path).split('.')[0] + '\n')
+        for item in x_values:
+            f.write("%s\n" % item)
+        for item in y_values:
+            f.write("%s\n" % item)
 
-print("mltool (pyscripts): [BİLGİ] veri seti yaratıldı.")
-print("Konum: %s" % fileloc)
+    print("mltool (pyscripts): [BİLGİ] veri seti yaratıldı.")
+    print(f"Konum: {file_path}")
+
+
+main()
