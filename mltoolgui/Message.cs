b196f5411e12308fb02731bbea0281d2e1528db8
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2021-12-19 00:13:07 +0300

    Rename ShowDialog() to Show()

--- mltoolgui/Message.cs
+++ mltoolgui/Message.cs
@@ -17,13 +17,13 @@
             ButtonCancel.Visible = cancellable;
         }
 
-        public static DialogResult ShowDialog(string title, string message)
+        public static DialogResult Show(string title, string message)
         {
             using Message m = new(title, message);
             return m.ShowDialog();
         }
 
-        public static DialogResult ShowDialog(string title, string message, bool cancellable = true)
+        public static DialogResult Show(string title, string message, bool cancellable = true)
         {
             using Message m = new(title, message, cancellable);
             return m.ShowDialog();
